9f5aef662dc0229c2f321693cf3c4c567dc86206
DanhNPI <danh.do@lginnotek.com>
2022-06-03 19:56:01 +0700

    Toán tử 3 ngôi

--- conditions.py
+++ conditions.py
@@ -3,3 +3,14 @@ num = int(input('Nhập số nguyên: '))
 result = "Even" if num%2 == 0 else "Odd"
 
 print (result)
+
+'-------------------------------------------'
+
+num = float(input('Nhập số: '))
+
+if num%2 == 0:
+    print('Even')
+elif num%2 == 1:
+    print ('Odd')
+else:
+    print ('Not Even Not Odd!')
